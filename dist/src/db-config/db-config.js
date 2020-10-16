d10ec8a3183ec9b3303428e73de65f9f6a9c1f19
Sriraj G <srirajg@deloitte.com>
2020-10-16 22:04:47 +0530

    updated git igore

--- dist/src/db-config/db-config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pg_1 = require("pg");
-exports.default = new pg_1.Pool({
-    max: 20,
-    connectionString: 'postgres://admin:admin1234@localhost:5433/sub-ji',
-    idleTimeoutMillis: 30000
-});
-//# sourceMappingURL=db-config.js.map
